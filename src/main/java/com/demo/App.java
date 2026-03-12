30c18f030212a90b8f9f7b2852724dd1a6225170
Ubuntu <ubuntu@ip-172-31-42-108.ap-south-1.compute.internal>
2026-03-12 07:27:37 +0000

    Initial Maven test project

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,9 @@
+package com.demo;
+
+public class App {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/test/java/com/demo/AppTest.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        App app = new App();
+        assertEquals(5, app.add(2,3));
+    }
+
+}
